984915226e9c98a4cd612e7c16b6a36b51ddb676
Matt Huston <matthewhuston@gmail.com>
2021-05-06 03:18:33 -0600

    Cheers to @dev-est and @dvdim for this
    awesome little tray app

--- switch.bash
+++ switch.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 # just a stupid wrapper to exec the below, log the output, and run in the background.
+# just chmod this to 755 and alias it if you want quick access
 cd /System/Volumes/Data/DATA/DEV/tuya_tray
 /usr/local/opt/python@3.7/bin/python3  tuya-tray.py 2>&1 | tee switch_output.log
 exit
